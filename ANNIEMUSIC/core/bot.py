a0ead0bc6ddd48891dc41bac9bcabbe9ccc655a0
Rudra <159684523+RudraXbot@users.noreply.github.com>
2024-02-25 15:31:35 +0530

    Update bot.py

--- ANNIEMUSIC/core/bot.py
+++ ANNIEMUSIC/core/bot.py
@@ -8,9 +8,9 @@ from ..logging import LOGGER
 
 class JARVIS(Client):
     def __init__(self):
-        LOGGER(__name__).info(f"Annie is on the way...")
+        LOGGER(__name__).info(f"Rudra is on the way...")
         super().__init__(
-            name="𝐀𝐍𝐍𝐈𝐄 𝐗 𝐌𝐔𝐒𝐈𝐂",
+            name="𝐑𝐔𝐃𝐑𝐀 𝐗 𝐌𝐔𝐒𝐈𝐂",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
